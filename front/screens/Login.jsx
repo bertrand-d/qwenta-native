2dc4af5da2d65d34bc59f18a8eda9f715e797807
reuby-dev <delphineb57@gmail.com>
2024-07-16 20:09:31 +0200

    add tailwind

--- front/screens/Login.jsx
+++ front/screens/Login.jsx
@@ -8,14 +8,14 @@ export default function Login() {
     const [email, setEmail] = useState("");
 
     return (
-        <View style={style.loginView}>
+        <View className="flex-1 items-center justify-center bg-blue">
             <View style={style.loginContainer}>
                 <View style={style.iconContainer}>
                     <Image source={require('@/assets/images/user.png')} />
                 </View>
                 <View style={style.whiteContainer}>
                     <MyText style={style.fontTitle}>Connexion</MyText>
-                    <MyText>Connectez-vous grâce à votre adresse email</MyText>
+                    <MyText>Connectez-vous grâce à votre adresse e-mail</MyText>
                     <MyText style={style.labelTextInput}>Adresse e-mail</MyText>
                     <TextInput style={style.textInput} onChangeText={(text) => setEmail(text)} value={email}></TextInput>
                     <MyButton title="Se connecter"></MyButton>
